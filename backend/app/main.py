c0bc825f951c340d7ffa2869216ec7ac332b8e4a
Shaurya_Official <shauryaofficial06@gmail.com>
2026-09-25 12:34:03 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,10 +18,10 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
+    allow_origins = [
         "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
+        "https://complaint-system-sepia-xi.vercel.app/",
+    ]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
